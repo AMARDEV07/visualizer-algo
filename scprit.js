acf71cf0aee51b948fb5a1a00e0220b2e617c8c5
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2025-04-03 23:03:14 +0530

    Add files via upload

---
+++ scprit.js
@@ -0,0 +1,19 @@
+ 
+
+     function showsidebar() {
+     const sideBar = document.querySelector('.sidebar');
+      sideBar.style.display = 'flex';  // Sidebar ko show karne ke liye display ko flex set karna
+     }
+
+
+
+    function ClosesildeBar() {
+     const sideBar = document.querySelector('.sidebar');
+     sideBar.style.display = 'none';  // Sidebar ko hide karne ke liye display ko none set karna
+    }
+
+
+
+    
+    
+  
